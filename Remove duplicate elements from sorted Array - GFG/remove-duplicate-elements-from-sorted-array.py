2a1140ea1b4be159d1a6abd7d471d0e9685c38b5
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2023-06-02 12:08:03 +0530

    Added solution - LeetHub

---
+++ Remove duplicate elements from sorted Array - GFG/remove-duplicate-elements-from-sorted-array.py
@@ -0,0 +1,29 @@
+#User function template for Python
+
+class Solution:	
+	def remove_duplicate(self,arr, N):
+		i = 0
+		for j in range(N):
+		    if arr[j]!=arr[i]:
+		        i+=1
+		    
+		    arr[i] = arr[j]
+		return i+1    
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        N = int(input())
+        A = list(map(int, input().strip().split()))
+        ob = Solution()
+        n = ob.remove_duplicate(A,N)
+        for i in range(n):
+            print(A[i], end=" ")
+        print()
+
+
+# } Driver Code Ends
