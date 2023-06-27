766d6b3d04762829749065c44b886ba3c419e01d
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2023-06-27 12:10:31 +0530

    Added solution - LeetHub

---
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+	def pushZerosToEnd(self,arr, n):
+    	cnt=0
+    	for i in range(n):
+    	    if arr[i]==0:
+    	        continue
+    	    
+    	    arr[i],arr[cnt]=arr[cnt],arr[i]
+    	    cnt +=1
+    	    
+    	
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.pushZerosToEnd(arr, n)
+        for x in arr:
+            print(x, end=" ")
+        print()
+        tc -= 1
+# } Driver Code Ends
